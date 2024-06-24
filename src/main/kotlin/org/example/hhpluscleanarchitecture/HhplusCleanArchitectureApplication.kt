67746ba4c4262173e9fd345e76ff92fd9da5789e
MinKyuSong <dev.isidore@gmail.com>
2024-06-24 22:09:57 +0900

    initial commit

---
+++ src/main/kotlin/org/example/hhpluscleanarchitecture/HhplusCleanArchitectureApplication.kt
@@ -0,0 +1,11 @@
+package org.example.hhpluscleanarchitecture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HhplusCleanArchitectureApplication
+
+fun main(args: Array<String>) {
+    runApplication<HhplusCleanArchitectureApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/hhpluscleanarchitecture/HhplusCleanArchitectureApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.hhpluscleanarchitecture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HhplusCleanArchitectureApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
